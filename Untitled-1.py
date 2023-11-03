17b8c4e7c44e83f32a235ca3043665b1991a654e
vele4099 <142831267+vele4099@users.noreply.github.com>
2023-11-03 16:07:47 +0000

    start

---
+++ Untitled-1.py
@@ -0,0 +1,13 @@
+import sympy as sp
+import math as  mt
+
+print(sp.sqrt(2) ** 2)
+print(mt.sqrt(2) ** 2)
+
+#Function Linear Equations
+def linear_solver():
+    pass 
+
+#Function Quadratic Equations
+def Quadratic():
+    pass 
